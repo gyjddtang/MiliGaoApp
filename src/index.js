a32ca68de5128f17d019cdafba1a8dcc78e55d90
gyjddtang <805793774@qq.com>
2019-01-21 16:08:57 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 /**
- * Created by MiliGao on 2019-01-21.
+ * Sample React Native Index
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
  */
-import * as React from 'react';
-import { View, Text } from 'react-native';
-class Index extends React.Component {
-    render() {
-        return (React.createElement(View, null,
-            React.createElement(Text, null, "hello, typescript")));
-    }
+
+import React, {Component} from 'react';
+import {View} from 'react-native';
+import Home from './screens/home';
+
+class Index extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Home />
+      </View>
+    );
+  }
 }
+
 export default Index;
+
